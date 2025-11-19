95daab03048970653773328a03ac7d4558026c92
FabricioJim <yofabris1234@gmail.com>
2025-11-19 16:54:16 -0600

    Botones de agregar carpeta y contraseña

--- LogicaYMetodos/Main.py
+++ LogicaYMetodos/Main.py
@@ -6,13 +6,6 @@ class Control:
         self.arbol = ArbolGeneral()
         self.carpetaActual = self.arbol.raiz
 
-    def main(self):
-        self.arbol.agregarContraseña("Amazon", "Fabricio", "12345", None)
-        self.arbol.agregarContraseña("Prime", "Damian", "54321", None)
-        self.arbol.agregarContraseña("Computrabajo", "Ana", "aaaaa", None)
-        self.arbol.agregarCarpeta("Redes sociales", None)
-        self.arbol.agregarCarpeta("Cuentas bancarias", None)
-
 
 # para que no se ejecute al ser importado, solo cunado otro lo ocupe
 if __name__ == "__main__":
